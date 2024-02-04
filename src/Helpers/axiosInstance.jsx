ed8570fef6592396c3343d5625cf25b740581536
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2024-02-05 00:39:26 +0530

    axiosInstance created

---
+++ src/Helpers/axiosInstance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function axiosInstance() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default axiosInstance
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import './index.css'
 
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
+        <Toaster />
     </BrowserRouter>
 )
