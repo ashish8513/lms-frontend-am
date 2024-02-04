41c808bf0566a413c6717cd4231733cdb60ddfda
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2024-02-05 00:25:32 +0530

    configuration of eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
 <>
-  lms
+<Routes>
+  {/* <Route path="/" element={<Home />}>< Route /> */}
+
+</Routes>
 </>
   )
 }
